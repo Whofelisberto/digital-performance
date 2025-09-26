8b43b2bf0a441164aa8367da32ccf3cdce3e16d2
Whofelisberto <felisberto563@gmail.com>
2025-09-26 17:37:03 -0300

    Salvando alterações locais

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from controllers.api_controller import api_bp
+from controllers.web_controller import web_bp
 
-app = Flask(__name__)
-app.secret_key = "chave-super-secreta"  # troque em produção
+app = Flask(__name__, template_folder="frontend/templates")
+app.secret_key = "chave-super-secreta"
 
 app.register_blueprint(api_bp)
+app.register_blueprint(web_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
